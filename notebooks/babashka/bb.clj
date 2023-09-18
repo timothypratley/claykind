526ffb55e215de5073b307675f30d8cc23913eca
daslu <daniel.slutsky@gmail.com>
2023-09-18 21:32:45 +0300

    added some structure to the bb example notebook

--- notebooks/babashka/bb.clj
+++ notebooks/babashka/bb.clj
@@ -4,11 +4,22 @@
 
 ;; Babashka is, by design, as close to Clojure as possible.
 
+;; ## Basic Examples
+
+(+ 1 2)
+
+{:x (range 3)}
+
+;; ## Time
 (def now (java.time.ZonedDateTime/now))
 (def LA-timezone (java.time.ZoneId/of "America/Los_Angeles"))
 (def LA-time (.withZoneSameInstant now LA-timezone))
 (def pattern (java.time.format.DateTimeFormatter/ofPattern "HH:mm"))
-(println (.format LA-time pattern))
+(.format LA-time pattern)
+
+;; ## Babashka vs clojure
+
+;; ##
 
 ;; The notable differences are:
 
@@ -16,11 +27,17 @@
 ;; * Not all of Clojure is available
 ;; * Files start with a shell directive instead of a namespace
 
+;; ##
+
 #! Did you know that Clojure treats `#!` as a comment?
+
+;; ##
 
 ;; So you can already create Babashka notebooks if you ignore the differences,
 ;; but this project (claykind) will detect Babashka and use Sci,
 ;; which will make it more directly compatible.
+
+;; ##
 
 ;; Would it be interesting thing to try is running claykind from babashka?
 ;; What possibilities does that open up?
