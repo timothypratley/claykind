e30f17eae4e6240ceecf8e40db91cfa67a9ee619
daslu <daniel.slutsky@gmail.com>
2023-09-21 01:08:14 +0300

    updated babashka examples

--- babashka-example-book/notebooks/git_log_analysis.clj
+++ babashka-example-book/notebooks/git_log_analysis.clj
@@ -84,6 +84,7 @@ freqs-plot
 
 (portal-widget freqs-plot)
 
+;; --------------------------------------------------
 ;; ![babashka](https://avatars.githubusercontent.com/u/64927540?s=200&v=4){height=128}
 ;; ![claykind](https://raw.githubusercontent.com/scicloj/graphic-design/live/icons/Clay.svg){height=128}
 ;; ![portal](https://raw.githubusercontent.com/djblue/portal/master/resources/splash.svg){height=128}
